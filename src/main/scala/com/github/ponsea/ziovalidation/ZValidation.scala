6cfb06aa77669a057a8f2f3298560a4e7c1f5acd
ponsea <puodsea@gmail.com>
2020-03-09 01:26:56 +0900

    Add ZIOValidationOps#cozip

--- src/main/scala/com/github/ponsea/ziovalidation/ZValidation.scala
+++ src/main/scala/com/github/ponsea/ziovalidation/ZValidation.scala
@@ -1,11 +1,11 @@
 package com.github.ponsea.ziovalidation
 
-import zio.{ Exit, UIO, ZIO }
+import zio.{ UIO, ZIO }
 
 object ZValidation {
   // same as ZIO.mapParN except that await all results and combine errors
   def mapParN[R, E, A, B, C](zio1: ZIO[R, E, A], zio2: ZIO[R, E, B])(f: (A, B) => C): ZIO[R, E, C] = {
-    zio1.validateWithPar(zio2)(f)
+    zio1.cozipWithPar(zio2)(f)
   }
 
   def mapParN[R, E, A, B, C, D](zio1: ZIO[R, E, A], zio2: ZIO[R, E, B], zio3: ZIO[R, E, C])(
@@ -27,30 +27,44 @@ object ZValidation {
     }
   }
 
-  implicit class ZIOOps[-R, +E, +A](self: ZIO[R, E, A]) {
-    // same as ZIO#zipWithPar except that await results and combine errors
-    def validateWithPar[R1 <: R, E1 >: E, B, C](that: ZIO[R1, E1, B])(f: (A, B) => C): ZIO[R1, E1, C] = {
+  implicit class ZIOValidationOps[-R, +E, +A](self: ZIO[R, E, A]) {
+    // same as ZIO#zipWith except that await all results and combine errors
+    def cozipWith[R1 <: R, E1 >: E, B, C](that: ZIO[R1, E1, B])(f: (A, B) => C): ZIO[R1, E1, C] = {
+      self.foldCauseM(
+        cause1 => that.foldCauseM(cause2 => ZIO.halt(cause1 ++ cause2), _ => ZIO.halt(cause1)),
+        a => that.foldCauseM(cause => ZIO.halt(cause), b => UIO(f(a, b)))
+      )
+    }
+
+    // same as ZIO#zip except that await all results and combine errors
+    def cozip[R1 <: R, E1 >: E, B](that: ZIO[R1, E1, B]): ZIO[R1, E1, (A, B)] = {
+      cozipWith(that)((a, b) => (a, b))
+    }
+
+    // alias for `cozip`
+    def |||[R1 <: R, E1 >: E, B](that: ZIO[R1, E1, B]): ZIO[R1, E1, (A, B)] = {
+      cozip(that)
+    }
+
+    // same as ZIO#zipWithPar except that await all results and combine errors
+    def cozipWithPar[R1 <: R, E1 >: E, B, C](that: ZIO[R1, E1, B])(f: (A, B) => C): ZIO[R1, E1, C] = {
       for {
         fiber1 <- self.fork
         fiber2 <- that.fork
         exit1  <- fiber1.await
         exit2  <- fiber2.await
-        result <- exitToZIO(exit1 <&> exit2)
+        result <- ZIO.done(exit1 <&> exit2)
       } yield f.tupled(result)
     }
 
-    // same as ZIO#zipPar except that await results and combine errors
-    def validatePar[R1 <: R, E1 >: E, B](that: ZIO[R1, E1, B]): ZIO[R1, E1, (A, B)] = {
-      validateWithPar(that)((a, b) => (a, b))
+    // same as ZIO#zipPar except that await all results and combine errors
+    def cozipPar[R1 <: R, E1 >: E, B](that: ZIO[R1, E1, B]): ZIO[R1, E1, (A, B)] = {
+      cozipWithPar(that)((a, b) => (a, b))
     }
 
     // alias for `validatePar`
     def <|>[R1 <: R, E1 >: E, B](that: ZIO[R1, E1, B]): ZIO[R1, E1, (A, B)] = {
-      self.validatePar(that)
+      self.cozipPar(that)
     }
-  }
-
-  private def exitToZIO[E, A](exit: Exit[E, A]) = {
-    exit.foldM(ZIO.halt(_), UIO(_))
   }
 }
